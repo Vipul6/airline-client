cbbfafdecaefa204cc98ac9b6a1fd99785be7fc3
Rahul Mehta <cruzer.mehta.97@gmail.com>
2020-01-05 19:36:19 +0530

    Lazy loading added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
-import "./App.css";
-import Home from "./components/Home";
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+const AsyncHome = lazy(() => import("./components/Home"));
+const AsyncAbout = lazy(() => import("./components/About"));
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Router>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </div>
+        <Switch>
+          <Route exact path="/" component={AsyncHome} />
+          <Route path="/about" component={AsyncAbout} />
+        </Switch>
+      </Router>
+    </Suspense>
   );
-}
+};
 
 export default App;
