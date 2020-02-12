3fc1d3d46eacbda0ec1ac200386aa316b868ca1d
m1050954 <m1050954@mindtree.com>
2020-02-12 17:49:40 +0530

    heart

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "../styles/footer.scss";
 
+const heartIcon = require("../assets/images/heart-icon.png");
+
 const Footer = () => {
-  return <div className="footer-container">Footer</div>;
+  return (
+    <div className="footer-container">
+      <span className="footer-text">
+        Developed with
+        <img src={heartIcon} alt="heart" className="heart-icon" />
+        by Rahul Mehta
+      </span>
+    </div>
+  );
 };
 
 export default Footer;
