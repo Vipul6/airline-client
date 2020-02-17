6e73797ed6f000e72eaffd4958e5dd25452a3948
m1050954 <m1050954@mindtree.com>
2020-02-17 18:28:04 +0530

    about

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,77 @@
 import React from "react";
 import Carousel from "./Carousel";
+import "../styles/about.scss";
 
 const About = props => {
   const routeParams = props.match.path;
   if (routeParams === "/about") {
     props.updateActiveLink("about");
   }
+
+  const history = () => {
+    return (
+      <div className="history-wrapper">
+        <span className="about-title">Who we are ?</span>
+        <span className="history-content">
+          <span className="para-title"> Cruzer Airlines </span> was incorporated
+          in February 2020 in Bangalore, India. The Company is one of the fast
+          growing & profitable travel company in Asia's online space driven by
+          technology, better buyingdue to demand aggregation & forward cash
+          flows. The Company is a leading distributor of transportation
+          ticketing, accommodation reservation,packaged tours, corporate travel
+          management, travel ancillaries, retail and financial services. The
+          compant offers a comprehensive suite of travel & retail products and
+          growing set of financial services products, provided by 10,000+
+          suppliers on the platform. The company offers its services through a
+          robust, highly scalable cloud based technology platform consisting of
+          mobile apps, websites, corporate platform, and a centralized,
+          toll-free, 24-hour customer service center.
+        </span>
+        <span className="history-content">
+          <span className="para-title"> Cruzer Airlines's </span>
+          Corporate Travel Management Business unit helps corporate clients
+          effectively manage their travel needs and significantly reduce travel
+          expenses. Cruzer Airlines expanded its footprints to Philippines,
+          Indonesia, Singapore and UAE. Today, Via's network include over
+          100,000 active travel partners across 2,600 towns and cities & over
+          13,000 pin codes across Asia and over 7000+ signed SMEs.
+        </span>
+        <div className="paragraph-wrapper">
+          <div className="para-holder">
+            <span className="para-title">We make selling very easy.</span>
+            <span className="para-content">
+              We started with the premise of selling travel, not necessarily
+              under our brand, but to everyone through our partners, world class
+              travel products and services in every neighborhood and deliver
+              great customer service. We serve over 2 million customers that
+              walk into our outlets everyday, that you can practically find in
+              your neighborhood. We are the
+              <span className="para-title"> Cruzer Airlines </span>
+              Inside Of the Travel World.
+            </span>
+          </div>
+          <div className="para-holder">
+            <span className="para-title">Unbelievable customer service.</span>
+            <span className="para-content">
+              NO customer calls when all is well. We believe customer service
+              starts with listening to the customer, owning what is wrong and
+              then going out of your way to fix it. That's it, it does not mean,
+              large call centers flooded with hundreds of souls or jazzy 1-800
+              numbers. Just a simple email or SMS will do. We will call you
+              back. We fundamentally believe most problems can be eliminated by
+              design and the rest by listening.
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  };
   return (
     <div className="snackbar-alignment">
-      about
-      <Carousel />
+      {history()}
+      <div className="carousel-container">
+        <Carousel />
+      </div>
     </div>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,20 @@ import Axios from "axios";
 import Snackbar from "./Snackbar";
 import { setUserDetails } from "../redux/actions";
 import { useDispatch } from "react-redux";
+import "../styles/home.scss";
+
+const imgArray = [
+  require("../assets/images/mysore-destination.jpg"),
+  require("../assets/images/goa-destination.jpg"),
+  require("../assets/images/shimla-destination.jpg"),
+  require("../assets/images/jaipur-destination.jpg"),
+  require("../assets/images/mumbai-destination.jpg"),
+  require("../assets/images/delhi-destination.jpg"),
+  require("../assets/svg/search.svg"),
+  require("../assets/svg/wallet.svg"),
+  require("../assets/svg/dollar.svg"),
+  require("../assets/svg/instant.svg")
+];
 
 const Home = props => {
   const [snackbar, updateSnackbar] = useState({ showSnackbar: false });
@@ -65,12 +79,137 @@ const Home = props => {
     props.updateActiveLink("home");
   }
 
+  const destinations = () => {
+    return (
+      <div className="destination-wrapper">
+        <div className="destination-content">
+          <div className="img-content">
+            <img
+              loading="eager"
+              src={imgArray[0]}
+              alt="Mysore"
+              className="dest-img"
+            />
+            <span className="dest-label">Mysore</span>
+          </div>
+          <div className="img-content">
+            <img
+              loading="eager"
+              src={imgArray[1]}
+              alt="Goa"
+              className="dest-img"
+            />
+            <span className="dest-label">Goa</span>
+          </div>
+        </div>
+
+        <div className="destination-content">
+          <div className="img-content">
+            <img
+              loading="eager"
+              src={imgArray[2]}
+              alt="Shimla"
+              className="dest-img"
+            />
+            <span className="dest-label">Shimla</span>
+          </div>
+          <div className="img-content">
+            <img
+              loading="eager"
+              src={imgArray[3]}
+              alt="Jaipur"
+              className="dest-img"
+            />
+            <span className="dest-label">Jaipur</span>
+          </div>
+        </div>
+
+        <div className="destination-content">
+          <div className="img-content">
+            <img
+              loading="eager"
+              src={imgArray[4]}
+              alt="Mumbai"
+              className="dest-img"
+            />
+            <span className="dest-label">Mumbai</span>
+          </div>
+          <div className="img-content">
+            <img
+              loading="eager"
+              src={imgArray[5]}
+              alt="Delhi"
+              className="dest-img"
+            />
+            <span className="dest-label">Delhi</span>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const topContent = () => {
+    return (
+      <div className="bottom-content-wrapper">
+        <div className="company-content">
+          <img
+            loading="eager"
+            src={imgArray[6]}
+            alt="search"
+            className="svg-icon"
+          ></img>
+          <span className="benefits">Look no further</span>
+          <span>Search all online travel deals in one go</span>
+        </div>
+        <div className="company-content">
+          <img
+            loading="eager"
+            src={imgArray[7]}
+            alt="wallet"
+            className="svg-icon"
+          ></img>
+          <span className="benefits">Shop with confidence</span>
+          <span>No hidden fees, taxes or other nasty surprises</span>
+        </div>
+        <div className="company-content">
+          <img
+            loading="eager"
+            src={imgArray[8]}
+            alt="money"
+            className="svg-icon"
+          ></img>
+          <span className="benefits">Pay the way you want</span>
+          <span>See only sellers who support your preferred methods</span>
+        </div>
+        <div className="company-content">
+          <img
+            loading="eager"
+            src={imgArray[9]}
+            alt="instant"
+            className="svg-icon"
+          ></img>
+          <span className="benefits">Instant booking</span>
+          <span>For selected sellers, book with just a couple of clicks</span>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <React.Fragment>
       <div className="snackbar-alignment">
         {snackbar.showSnackbar ? snackbar.snackbarContent : null}
       </div>
-      <div>Home</div>
+      <React.Fragment>
+        <span className="destination-title">Why Cruzer Airlines?</span>
+        {topContent()}
+      </React.Fragment>
+      <React.Fragment>
+        <span className="destination-title">
+          Top popular destination in India
+        </span>
+        {destinations()}
+      </React.Fragment>
     </React.Fragment>
   );
 };
